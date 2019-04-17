b882e45eb193d1e3c725e8dc3e0af4d52b139428
b-antonenko <47101465+b-antonenko@users.noreply.github.com>
2019-04-17 18:03:08 +0300

    Add files via upload

---
+++ double num.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function isDouble(num) {
+    return num % 2 === 0 ? true : false;
+};
+
+
+console.log(isDouble(22));
+console.log(isDouble(11));
+console.log(isDouble(10));
+console.log(isDouble(101));
